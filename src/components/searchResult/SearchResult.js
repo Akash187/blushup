1a72c4e2ef39bcb388cea4db6932657f8eea0c6d
Akash Kumar Seth <kumarakashseth007@gmail.com>
2019-05-13 14:45:17 +0530

    Added Pagination to search result.

--- src/components/searchResult/SearchResult.js
+++ src/components/searchResult/SearchResult.js
@@ -1,32 +1,52 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import SelectSortBy from "../layouts/SelectSortBy";
 import ShopInfo from "./ShopInfo";
 import MapContainer from "../layouts/MapContainer";
+import ReactPaginate from 'react-paginate';
 
 const SearchResult = () => {
+
   return (
-    <div className='search-results'>
-      <div className="search-result">
-        <div className="search-result-header">
-          <span className="search-result-count">18 stores available</span>
-          <div className='search-result-dropdown'>
-            <SelectSortBy/>
-            <i className="search-result-dropdown-icon fas fa-caret-down"/>
+    <Fragment>
+      <div className='search-results'>
+        <div className="search-result">
+          <div className="search-result-header">
+            <span className="search-result-count">18 stores available</span>
+            <div className='search-result-dropdown'>
+              <SelectSortBy/>
+              <i className="search-result-dropdown-icon fas fa-caret-down"/>
+            </div>
+          </div>
+          <div className="search-result-shops">
+            <ShopInfo/>
+            <ShopInfo/>
+            <ShopInfo/>
+            <ShopInfo/>
+            <ShopInfo/>
+            <ShopInfo/>
           </div>
         </div>
-        <div className="search-result-shops">
-          <ShopInfo/>
-          <ShopInfo/>
-          <ShopInfo/>
-          <ShopInfo/>
-          <ShopInfo/>
-          <ShopInfo/>
+        <div className="search-result-map">
+          <MapContainer/>
         </div>
       </div>
-      <div className="search-result-map">
-        <MapContainer/>
+      <div className="result-pagination">
+        <ReactPaginate
+          previousLabel={ <i className="fas fa-chevron-circle-left"/>}
+          nextLabel={<i className="fas fa-chevron-circle-right"/>}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={10}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={3}
+          onPageChange={(data) => console.log(data)}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
       </div>
-    </div>
+      <br/>
+    </Fragment>
   );
 };
 
